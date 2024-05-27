14cea4525c14eb0ea448461f97e06ddd3378a5dd
Paolo <legotkinc2006@gmail.com>
2024-05-27 11:07:58 +0300

    Разработанный контекст для загрузки данных. Galkin

---
+++ API_Galkin/Context/TaskContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using API_Galkin.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace API_Galkin.Context
+{
+    public class TaskContext : DbContext
+    {
+        public DBSet<Tasks> Tasks { get; set; }
+
+        public TaskContext()
+        {
+            Database.EnsureCreated();
+            Tasks.Load();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) =>
+            optionsBuilder.UseMySql("server=127.0.0.1;" +
+                "uid=root;" +
+                "pwd=;" +
+                "database=TaskManager",
+                new MySqlServerVersion(new Version(8, 0, 11)));
+    }
+}
