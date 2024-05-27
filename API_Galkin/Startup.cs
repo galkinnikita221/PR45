184d550b2196bf9e4503616556e378299bd796c4
Paolo <legotkinc2006@gmail.com>
2024-05-27 11:01:02 +0300

    Настройка проекта, подключение swagger. Galkin

--- API_Galkin/Startup.cs
+++ API_Galkin/Startup.cs
@@ -3,8 +3,10 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -14,23 +16,29 @@ namespace API_Galkin
     {
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc(option => option.EnableEndpointRouting = false);
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "Руководство для использованя запросов",
+                    Description = "Полное руководство для использования запросов, находящихся в проекте"
+                });
+                var filePath = Path.Combine(System.AppContext.BaseDirectory, "ASP_GET.xml");
+                c.IncludeXmlComments(filePath);
+            });
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
+            app.UseStatusCodePages();
+            app.UseDeveloperExceptionPage();
+            app.UseMvcWithDefaultRoute();
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
             {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.UseRouting();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Запросы GET");
             });
         }
     }
