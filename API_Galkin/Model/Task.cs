36f8519bd40b3440352dc6fe77e0f3f589a012b1
Paolo <legotkinc2006@gmail.com>
2024-05-27 11:02:35 +0300

    Разработанные модели данных. Galkin

---
+++ API_Galkin/Model/Task.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API_Galkin.Model
+{
+    public class Task
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Priority { get; set; }
+        public DateTime DataExecute { get; set; }
+        public string Comment { get; set; }
+        public bool Done { get; set; }
+    }
+}
